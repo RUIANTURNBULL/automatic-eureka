6f7a18c0cd7134701a7b609bc45d85095e5604d2
RUIANTURNBULL <IAN_TURNBULL@OUTLOOK.COM>
2017-09-17 20:32:28 -0400

    Add files via upload

---
+++ Control.java
@@ -0,0 +1,9 @@
+package project4;
+
+public class Control {
+private View view;
+
+public Control() {
+	view = new View();
+}
+}
---
+++ Driver.java
@@ -0,0 +1,10 @@
+package project4;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		new Control();
+
+	}
+
+}
